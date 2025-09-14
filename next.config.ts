1e051e2e1654290ec3ea36cb93ee8675eb5e9d1a
Susmoy Debnath <susmoydebnath05@gmail.com>
2025-09-15 04:28:53 +0600

    eat: Initial commit of Airbnb clone

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: 'images.unsplash.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
         ],
     },
 };

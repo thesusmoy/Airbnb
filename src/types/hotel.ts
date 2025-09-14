3c8d551ad240d94246c4029ecbe6c2416b285d2e
Susmoy Debnath <susmoydebnath05@gmail.com>
2025-09-15 03:20:13 +0600

    feat: Initial commit of Airbnb clone

---
+++ src/types/hotel.ts
@@ -0,0 +1,11 @@
+export type THotel = {
+    id: number;
+    name: string;
+    location: string;
+    price: number;
+    rating: number;
+    image: string;
+    guests: number;
+    beds: number;
+    baths: number;
+};
